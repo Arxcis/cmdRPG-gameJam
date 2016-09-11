9f92b75dad7080939de8d3a6750bdeb6949c3ff6
Arxcis <jonas.solsvik@gmail.com>
2016-09-11 09:39:33 +0200

    Merge is real

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cmdRPG-gameJam
+
 Repository for the GameJam on 10.09 - 11.09.2016
+
+The simple hardcoded version&lt;3
---
+++ main.cpp
@@ -0,0 +1,33 @@
+
+
+
+
+// Oppmøte i kronolgisk rekkefølge:
+//   - Jonas
+//   - Eldar
+//   - Amici
+//   - Nataniel
+//   - Magne
+
+#include <iostream>
+
+
+struct GameResources {
+
+}
+
+struct Location {
+
+}
+
+struct Player {
+
+}
+
+struct Enemy {
+
+}
+
+struct NPC {
+
+}
