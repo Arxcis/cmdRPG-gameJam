24a60ffed6cd5ddde534559e5ea426c92b09e6bb
Arxcis <jonas.solsvik@gmail.com>
2016-09-11 21:21:07 +0200

    Animation working !!! !!"# wohoooooo

--- main.cpp
+++ main.cpp
@@ -13,9 +13,9 @@
 //                single file. 
 
 
+#include <vector>
 #include <iostream>
-#include <ncurses.h>   // Have to add "-lncurses" in Build Settings under 
-                       //  other Linker flags.
+#include <string>
 
 #ifdef WIN32
 #include <windows.h>
@@ -25,35 +25,104 @@
 
 using namespace std;
 
+
+//// ----------------------------- STRUCTS ------------------------------////
+
+struct Window {
+
+
+    //  ------- Struct attributes -----------
+
+        // Here is the 2d-array that is responsible for holding the information
+        //  about the window-state at any given time. 
+
+    const vector<string> windowEmpty = {
+    
+    "                                                                     \n",
+    "                                                                     \n",
+    "                                                                     \n",
+    "                                                                     \n",
+    "                                                                     \n",
+    "                                                                     \n",
+    "                                                                     \n",
+    "                                                                     \n",
+    "                                                                     \n",
+    "                                                                     \n",
+    "                                                                     \n",
+    "                                                                     \n",
+    "                                                                     \n",
+    "                                                                     \n",
+    "                                                                     \n",
+    "                                                                     \n",
+    "                                                                     \n",
+    "                                                                     \n",
+    "                                                                     \n",
+    "                                                                     \n",
+    "\n\n\n\n"
+    };
+            // Arrays cannot be assigned to other arrays.
+    vector<string> windowState = windowEmpty;
+
+
+    // ---------- Struct methods  ---------
+
+    void clearWindow() {     windowState = windowEmpty;    }
+
+
+    // @param : int row - a number between 0-19, specifies which row to change
+    // @param : string line - inputs a string of length 71, the entire row.
+    // @return: void
+
+    // WORKS LIKE A CHARM !!
+    void setLine(int row, string line) {
+
+        windowState[row] = line;
+    }
+
+
+    // @param : int row - a number between 0-19, specifies which row to change
+    // @param : int column - a number between 0-70, starting post of the word.
+    // @param : string word - a word of any lenght to be inserted at position.
+    // @return: void
+
+    // WORKS like A CHARM !! 
+    void setWord(uint row, uint column, string word){
+
+        // Debug info
+        //cout << row << ", "<< column << ", " << word.length() << "\n"; 
+
+        for (int i = column, j= 0; i < (column + word.length()); i++, j++){
+
+            // Makes sure that the end of line is not overwritten.
+            if ( i < 71) {
+                //cout << word[j];
+                windowState[row][i] = word[j];
+            }
+
+        }
+    }
+
+    void coutWindow()  {
+
+        for (int i=0; i < 20; i++){
+            cout << windowState[i];
+        }
+
+    }
+
+}; 
+
+// ------------------------------ GLOBALS -----------------------------------//
+
+char g_key = ' ';
+Window w; 
+
+
 //// ----------------------- FUNCTION DECLARATIONS ------------------------////
 
 void clearScreen();
 void zzz();
-
 void openingTitle();
-void mainMenu();
-
-void newGame1();
-void newGame2();
-
-// ------------------------------ GLOBALS -----------------------------------//
-
-namespace global {
-    char key = ' ';
-} 
-
-//// ------------------------------ MAIN ----------------------------------////
-
-
-int main(){
-
-    // ----- INIT ncurses and openingSequence
-    initscr();
-    openingTitle();
-
-    return 0;
-}
-
 
 //// ------------------------ FUNCTION DEFINITIONS  ---------------------- ////
 
@@ -68,15 +137,15 @@ void zzz(int milliseconds)        // cross-platform sleep function
 
 void resetScreen(){
 
-    global::key = ' ';            // Resets the global key, so it works as 
+    g_key = ' ';            // Resets the global key, so it works as 
                                   //  as intended. 
-    for (int i=0; i < 35; i++) {  // Prints 35 \n - endline characters.
+    for (int i=0; i < 30; i++) {  // Prints 35 \n - endline characters.
         cout << '\n';             //  effectively wipes the screen clean.
     }
 }
 
 
-// ----------------------------- VIEWCARDS ----------------------------------//
+// --------------------------------- VIEWS ----------------------------------//
 //   Comment:
 //    The following functions represent one view.
 //     Each view - function has the following steps:
@@ -88,171 +157,54 @@ void resetScreen(){
 //    To make the game appear consistent across all views we have to     
 //     standardize the width and height of each view.
 //
-//   ASCII - resolution:  68 x 20 characters.
+//   ASCII - resolution:  70(+1 newline character) x 20 characters.
 // 
 
 
-void openingTitle(){
+void openingView(){
 
-    resetScreen();       // Always call resetScreen() on top of a card.
-
-cout<< "===================================================================\n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                  THE                                              \n"
-    << "                    WIZARDS                                        \n"
-    << "                     HOLLIDAY                                      \n"
-    << "                    ----------                                     \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                      Enter to continue.....       \n"
-    << "===================================================================\n";
-
-    cin.ignore(); cin.clear();
-    mainMenu();
-}
-
-void mainMenu(){
-
-    // Make sure that a valid key has been entered.
-    while(toupper(global::key) != 'N' && 
-          toupper(global::key) != 'Q'){
-
-        resetScreen();       // Always call resetScreen() on top of a card.
+    int count = 0;
 
 
-cout<< "===================================================================\n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                         MAIN MENU                                 \n"
-    << "                    -------------------                            \n"
-    << "                                                                   \n"
-    << "                       [N]EW GAME                                  \n"
-    << "                                                                   \n"
-    << "                       [Q]UIT                                      \n"
-    << "                                                                   \n"
-    << "                    -------------------                            \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                      [N , Q] + Enter              \n"
-    << "===================================================================\n";
-    cin >> global::key;  
-    cin.ignore(); cin.clear();
-
-    }   
-        // Switch descision statement after the while-loop is broken.
-    switch(toupper(global::key)){
-
-        case 'N':
-            newGame1();
-            break;
-
-        case 'Q':
-            openingTitle();
-            break;
-        default:
-            break;
-    }
-}
-
-void newGame1(){
-
-    resetScreen();       // Always call resetScreen() on top of a card.
-
-cout<< "===================================================================\n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "   In the year of 1399 an English magician serves as               \n"
-    << "    a luxury guard for an English noble’s son. At the              \n"
-    << "    age of 5, the son, full with pranks and the attitude           \n"
-    << "    of a spoiled brat, annoys the magician to insanity.            \n"
-    << "    Through sheer desperation and a longing for peace,             \n"
-    << "    the magician sets out on a vacation to Norway,                 \n"
-    << "    the only remote enough country he knows of, and                \n"
-    << "    where he hopes to find absolutely nothing that                 \n"
-    << "       can bother him but rain, wind and snow....                  \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "===================================================================\n";
-
-    cin.ignore(); cin.clear();
-
-    newGame2();
-}
-
-void newGame2(){
-
-    int y, x;          // Store position of cursor
-    resetScreen();       // Always call resetScreen() on top of a card.
-
-
-        // Here it would have been awesome with som sort of animation.
-        //  using sleep and line jumping in some sort of clever way.
-
-cout<< "===================================================================\n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "    Sailing the seas...                                            \n"
-    << "                                                                   \n"
-    << "                                                                   \n";
-    getyx(stdscr, y, x);              // save current pos
-
-cout<< "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "                                                                   \n"
-    << "===================================================================\n";
-
-    cout << x << " , " <<y;
-
-    int i;
     while(1){
 
-        move(y, 0);            // ncurses - move(x,y)  - moves the cursor.
+        resetScreen();
 
-        // Y U NO WORK MOVE(y, x) :'(  ???????????? 
+        w.setWord(7, 12, "The");
+        w.setWord(8, 14, "Wizard's");
+        w.setWord(9, 16, "Holiday");
 
-        clrtoeol();            // ncurses - clrtoeol() - clears a line.
-
-        if(i % 2 == 0){
-
-cout <<"       YORK  - - - - - - - -  >  BERGEN                            \n";
-
+        if(count % 2 == 0){
+            w.setWord(10, 16, "-----------            ");
+        } else {
+            w.setWord(10, 16, "            -----------");
         }
 
+        w.setWord(11, 17, "Copyright 2016");
+
+        w.coutWindow();
+
+        count ++;
+
         zzz(1000);
-        i++;
     }
-
-    cin.ignore(); cin.clear();
-
-    mainMenu();
 }
+
+
+//// ------------------------------ MAIN ----------------------------------////
+
+
+int main(){
+
+    openingView();
+
+    return 0;
+}
+
+
+
+
+
+
+
+
